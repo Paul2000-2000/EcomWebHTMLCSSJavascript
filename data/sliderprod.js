379f0362b0c387500216e9a635a0d864fc0eecef
Paul2000-2000 <alexapaul2000@yahoo.com>
2023-08-19 16:22:24 +0300

    finish design for nav 2 and 3 + slider

---
+++ data/sliderprod.js
@@ -0,0 +1,35 @@
+export const sliderprod = [
+    {
+        url:'assets/slider/slider0.jpg',
+        pover:'Shoes Fashion',
+        h1:'Come and Get it!',
+        punder:'BRAND NEW SHOES',
+        button:'Shop Now',
+        check:0
+    },
+    {
+        url:'assets/slider/slider1.jpg',
+        pover:'Quick Fashion',
+        h1:'Fit your Wardrobe',
+        punder:'WITH LUXURY ITEMS',
+        button:'Shop Now',
+        check:1
+    },
+    {
+        url:'assets/slider/slider2.jpg',
+        pover:'Quick offer',
+        h1:'Wooden Minimal Sofa',
+        punder:'EXTRA 50% OFF',
+        button:'Shop Now',
+        check:2
+    },
+    {
+        url:'assets/slider/slider3.jpg',
+        pover:'Best Deals',
+        h1:'Home Workout Accesories',
+        punder:'PUSH THE LIMIT',
+        button:'Shop Now',
+        check:3
+    }
+
+];
---
+++ scripts/slider.js
@@ -0,0 +1,48 @@
+import { sliderprod } from '../data/sliderprod.js';
+
+const slidercontentElement = document.querySelector('.js-slider');
+const radioButtons = document.querySelectorAll('.slidercontent-checkbuttons input[type="radio"]');
+const productElements = [];
+
+// Create product elements and store them in an array
+sliderprod.forEach(product => {
+    const productElement = document.createElement('div');
+    productElement.classList.add('product');
+    
+    productElement.innerHTML = `
+        <p class="slidercontent-p-overh1">${product.pover}</p>
+        <h1>${product.h1}</h1>
+        <p class="slidercontent-p-underh1">${product.punder}</p>
+        <button class="slidercontent-button">${product.button}</button>
+    `;
+    productElement.style.background = `url('${product.url}')`;
+    productElement.style.backgroundSize = 'cover'; // Set background size to cover
+    
+    // Initially hide all products
+    productElement.style.display = 'none';
+
+    productElements.push(productElement);
+    slidercontentElement.appendChild(productElement);
+});
+
+// Show the first product initially
+productElements[0].style.display = 'block';
+productElements[0].style.width = '100%';
+productElements[0].style.height = '100%';
+radioButtons[0].checked = true; // Check the first radio button initially
+
+// Add event listeners to radio buttons
+radioButtons.forEach((radio, index) => {
+    radio.addEventListener('change', () => {
+        // Hide all products
+        productElements.forEach(productElement => {
+            productElement.style.display = 'none';
+        });
+        
+        // Show the selected product and make it full
+        productElements[index].style.display = 'block';
+        productElements[index].style.width = '100%';
+        productElements[index].style.height = '100%';
+        
+    });
+});
